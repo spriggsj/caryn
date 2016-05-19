c2d4277dc6f23fd6b10ef637f4f1d7ab91d6a2ca
jason spriggs <jasonspriggs73@gmail.com>
2016-05-18 20:55:46 -0700

    post

---
+++ wp-content/themes/html5blank-stable-child/front-page.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+	
+	<header class="hero-banner">
+		<div class="container">
+			<h1>Run with me</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolores harum fugiat praesentium corrupti illo ducimus, velit non! Quod dolores harum fugiat praesentium corrupti illo ducimus, velit non! Quod dolores harum fugiat praesentium corrupti illo ducimus, velit non!</p>
+		</div>
+	</header>
+	
+	<section class="recent-post">
+		<?php echo do_shortcode( '[custom-loop]' ); ?>
+	</section>
+
+	<section id="buffer">
+		<div class="container">
+			<!--<h2 class="name-title">Abundant Living <span>Mommy</span> E Books</h2>
+			<ul id="books">
+				<li><a href="#"><img src="<?php //echo get_stylesheet_directory_uri(); ?>/img/detoxyourhomehandbook.jpg" class="img-responsive"></a></li>
+				<li><a href="#"><img src="<?php //echo get_stylesheet_directory_uri(); ?>/img/healthy-pregnancy-guide.jpg" class="img-responsive"></a></li>
+				<li><a href="#"><img src="<?php //echo get_stylesheet_directory_uri(); ?>/img/home-remedies-handbook.jpg" class="img-responsive"></a></li>
+				<li><a href="#"><img src="<?php //echo get_stylesheet_directory_uri(); ?>/img/natural-beauty-guide.jpg" class="img-responsive"></a></li>
+			</ul>-->
+		</div>
+	</section>
+
+	<section class="meal-post">
+		<!--<?php echo do_shortcode( '[meal-loop]' ); ?>-->
+	</section>
+
+<?php get_footer(); ?>
